ff384b2e34d370d7b3ceb7e4a6b1c50adc340ee7
Sammy <ThatSammyVR@gmail.com>
2025-06-02 13:58:55 +0200

    just in case|

--- Exam-Template/islands/db.tsx
+++ Exam-Template/islands/db.tsx
@@ -10,8 +10,15 @@ db.exec(`
         username TEXT NOT NULL,
         password TEXT NOT NULL
     );   
+    DROP TABLE IF EXISTS complaints;
+    CREATE TABLE complaints(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        complaint TEXT NOT NULL,
+        status TEXT NOT NULL
+    )
 `);
 
 export function createUser(username: string, password:string) {
     db.exec("INSERT INTO users (username, password) VALUES (?, ?)", {username, password});
 }
+
